4f0f562292c723253c7b8cae874bfe3b086d7f7c
Ofir Matasas <ofir.matasas@gmail.com>
2022-06-14 18:24:51 +0300

    Styling FormGameSettings

--- B22 Ex05 Alon 315856864 Ofir 205916729/CheckersWindowsApp/FormGameSettings.Designer.cs
+++ B22 Ex05 Alon 315856864 Ofir 205916729/CheckersWindowsApp/FormGameSettings.Designer.cs
@@ -30,7 +30,6 @@ namespace CheckersWindowsApp
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGameSettings));
-            this.labelPlayerOneName = new System.Windows.Forms.Label();
             this.labelPlayerTwoName = new System.Windows.Forms.Label();
             this.textBoxPlayerOneName = new System.Windows.Forms.TextBox();
             this.textBoxPlayerTwoName = new System.Windows.Forms.TextBox();
@@ -42,55 +41,52 @@ namespace CheckersWindowsApp
             this.radioButtonAgainstComputer = new System.Windows.Forms.RadioButton();
             this.radioButtonAgainstHuman = new System.Windows.Forms.RadioButton();
             this.buttonStartGame = new System.Windows.Forms.Button();
+            this.labelPlayerOneName = new System.Windows.Forms.Label();
             this.groupBoxBoardSize.SuspendLayout();
             this.groupBoxPlayersNames.SuspendLayout();
             this.SuspendLayout();
             // 
-            // labelPlayerOneName
-            // 
-            this.labelPlayerOneName.AutoSize = true;
-            this.labelPlayerOneName.Location = new System.Drawing.Point(36, 40);
-            this.labelPlayerOneName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelPlayerOneName.Name = "labelPlayerOneName";
-            this.labelPlayerOneName.Size = new System.Drawing.Size(145, 20);
-            this.labelPlayerOneName.TabIndex = 0;
-            this.labelPlayerOneName.Text = "First player\'s name:";
-            // 
             // labelPlayerTwoName
             // 
             this.labelPlayerTwoName.AutoSize = true;
+            this.labelPlayerTwoName.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlayerTwoName.ForeColor = System.Drawing.Color.Black;
             this.labelPlayerTwoName.Location = new System.Drawing.Point(36, 122);
             this.labelPlayerTwoName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPlayerTwoName.Name = "labelPlayerTwoName";
-            this.labelPlayerTwoName.Size = new System.Drawing.Size(169, 20);
+            this.labelPlayerTwoName.Size = new System.Drawing.Size(191, 19);
             this.labelPlayerTwoName.TabIndex = 1;
             this.labelPlayerTwoName.Text = "Second player\'s name:";
             // 
             // textBoxPlayerOneName
             // 
             this.textBoxPlayerOneName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxPlayerOneName.Location = new System.Drawing.Point(224, 35);
+            this.textBoxPlayerOneName.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPlayerOneName.Location = new System.Drawing.Point(254, 35);
             this.textBoxPlayerOneName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxPlayerOneName.Name = "textBoxPlayerOneName";
-            this.textBoxPlayerOneName.Size = new System.Drawing.Size(149, 26);
+            this.textBoxPlayerOneName.Size = new System.Drawing.Size(224, 27);
             this.textBoxPlayerOneName.TabIndex = 2;
             // 
             // textBoxPlayerTwoName
             // 
             this.textBoxPlayerTwoName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBoxPlayerTwoName.Location = new System.Drawing.Point(224, 117);
+            this.textBoxPlayerTwoName.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPlayerTwoName.Location = new System.Drawing.Point(254, 117);
             this.textBoxPlayerTwoName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxPlayerTwoName.Name = "textBoxPlayerTwoName";
-            this.textBoxPlayerTwoName.Size = new System.Drawing.Size(149, 26);
+            this.textBoxPlayerTwoName.Size = new System.Drawing.Size(224, 27);
             this.textBoxPlayerTwoName.TabIndex = 3;
             // 
             // radioButton6x6
             // 
             this.radioButton6x6.AutoSize = true;
+            this.radioButton6x6.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton6x6.ForeColor = System.Drawing.Color.Black;
             this.radioButton6x6.Location = new System.Drawing.Point(36, 29);
             this.radioButton6x6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton6x6.Name = "radioButton6x6";
-            this.radioButton6x6.Size = new System.Drawing.Size(67, 24);
+            this.radioButton6x6.Size = new System.Drawing.Size(74, 23);
             this.radioButton6x6.TabIndex = 4;
             this.radioButton6x6.Text = "6 x 6";
             this.radioButton6x6.UseVisualStyleBackColor = true;
@@ -98,10 +94,12 @@ namespace CheckersWindowsApp
             // radioButton10x10
             // 
             this.radioButton10x10.AutoSize = true;
+            this.radioButton10x10.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton10x10.ForeColor = System.Drawing.Color.Black;
             this.radioButton10x10.Location = new System.Drawing.Point(201, 29);
             this.radioButton10x10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton10x10.Name = "radioButton10x10";
-            this.radioButton10x10.Size = new System.Drawing.Size(85, 24);
+            this.radioButton10x10.Size = new System.Drawing.Size(94, 23);
             this.radioButton10x10.TabIndex = 5;
             this.radioButton10x10.Text = "10 x 10";
             this.radioButton10x10.UseVisualStyleBackColor = true;
@@ -110,10 +108,12 @@ namespace CheckersWindowsApp
             // 
             this.radioButton8x8.AutoSize = true;
             this.radioButton8x8.Checked = true;
+            this.radioButton8x8.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton8x8.ForeColor = System.Drawing.Color.Black;
             this.radioButton8x8.Location = new System.Drawing.Point(117, 29);
             this.radioButton8x8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton8x8.Name = "radioButton8x8";
-            this.radioButton8x8.Size = new System.Drawing.Size(67, 24);
+            this.radioButton8x8.Size = new System.Drawing.Size(74, 23);
             this.radioButton8x8.TabIndex = 6;
             this.radioButton8x8.TabStop = true;
             this.radioButton8x8.Text = "8 x 8";
@@ -121,31 +121,41 @@ namespace CheckersWindowsApp
             // 
             // groupBoxBoardSize
             // 
+            this.groupBoxBoardSize.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxBoardSize.BackgroundImage = global::CheckersWindowsApp.Properties.Resources.wood_label_background;
+            this.groupBoxBoardSize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.groupBoxBoardSize.Controls.Add(this.radioButton6x6);
             this.groupBoxBoardSize.Controls.Add(this.radioButton10x10);
             this.groupBoxBoardSize.Controls.Add(this.radioButton8x8);
+            this.groupBoxBoardSize.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxBoardSize.ForeColor = System.Drawing.Color.Black;
             this.groupBoxBoardSize.Location = new System.Drawing.Point(18, 22);
             this.groupBoxBoardSize.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBoxBoardSize.Name = "groupBoxBoardSize";
             this.groupBoxBoardSize.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBoxBoardSize.Size = new System.Drawing.Size(426, 71);
+            this.groupBoxBoardSize.Size = new System.Drawing.Size(489, 71);
             this.groupBoxBoardSize.TabIndex = 7;
             this.groupBoxBoardSize.TabStop = false;
             this.groupBoxBoardSize.Text = "Board Size";
             // 
             // groupBoxPlayersNames
             // 
+            this.groupBoxPlayersNames.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxPlayersNames.BackgroundImage = global::CheckersWindowsApp.Properties.Resources.wood_label_background;
+            this.groupBoxPlayersNames.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBoxPlayersNames.Controls.Add(this.textBoxPlayerTwoName);
+            this.groupBoxPlayersNames.Controls.Add(this.textBoxPlayerOneName);
             this.groupBoxPlayersNames.Controls.Add(this.radioButtonAgainstComputer);
             this.groupBoxPlayersNames.Controls.Add(this.radioButtonAgainstHuman);
             this.groupBoxPlayersNames.Controls.Add(this.labelPlayerOneName);
-            this.groupBoxPlayersNames.Controls.Add(this.textBoxPlayerOneName);
-            this.groupBoxPlayersNames.Controls.Add(this.textBoxPlayerTwoName);
             this.groupBoxPlayersNames.Controls.Add(this.labelPlayerTwoName);
+            this.groupBoxPlayersNames.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxPlayersNames.ForeColor = System.Drawing.Color.Black;
             this.groupBoxPlayersNames.Location = new System.Drawing.Point(18, 102);
             this.groupBoxPlayersNames.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBoxPlayersNames.Name = "groupBoxPlayersNames";
             this.groupBoxPlayersNames.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBoxPlayersNames.Size = new System.Drawing.Size(426, 165);
+            this.groupBoxPlayersNames.Size = new System.Drawing.Size(489, 165);
             this.groupBoxPlayersNames.TabIndex = 8;
             this.groupBoxPlayersNames.TabStop = false;
             this.groupBoxPlayersNames.Text = "Players";
@@ -153,10 +163,12 @@ namespace CheckersWindowsApp
             // radioButtonAgainstComputer
             // 
             this.radioButtonAgainstComputer.AutoSize = true;
+            this.radioButtonAgainstComputer.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonAgainstComputer.ForeColor = System.Drawing.Color.Black;
             this.radioButtonAgainstComputer.Location = new System.Drawing.Point(138, 91);
             this.radioButtonAgainstComputer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButtonAgainstComputer.Name = "radioButtonAgainstComputer";
-            this.radioButtonAgainstComputer.Size = new System.Drawing.Size(104, 24);
+            this.radioButtonAgainstComputer.Size = new System.Drawing.Size(114, 23);
             this.radioButtonAgainstComputer.TabIndex = 7;
             this.radioButtonAgainstComputer.Text = "Computer";
             this.radioButtonAgainstComputer.UseVisualStyleBackColor = true;
@@ -166,10 +178,12 @@ namespace CheckersWindowsApp
             // 
             this.radioButtonAgainstHuman.AutoSize = true;
             this.radioButtonAgainstHuman.Checked = true;
+            this.radioButtonAgainstHuman.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonAgainstHuman.ForeColor = System.Drawing.Color.Black;
             this.radioButtonAgainstHuman.Location = new System.Drawing.Point(40, 91);
             this.radioButtonAgainstHuman.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButtonAgainstHuman.Name = "radioButtonAgainstHuman";
-            this.radioButtonAgainstHuman.Size = new System.Drawing.Size(86, 24);
+            this.radioButtonAgainstHuman.Size = new System.Drawing.Size(91, 23);
             this.radioButtonAgainstHuman.TabIndex = 8;
             this.radioButtonAgainstHuman.TabStop = true;
             this.radioButtonAgainstHuman.Text = "Human";
@@ -178,7 +192,11 @@ namespace CheckersWindowsApp
             // 
             // buttonStartGame
             // 
-            this.buttonStartGame.Location = new System.Drawing.Point(324, 275);
+            this.buttonStartGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonStartGame.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartGame.ForeColor = System.Drawing.Color.Black;
+            this.buttonStartGame.Image = global::CheckersWindowsApp.Properties.Resources.wood_label;
+            this.buttonStartGame.Location = new System.Drawing.Point(387, 277);
             this.buttonStartGame.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonStartGame.Name = "buttonStartGame";
             this.buttonStartGame.Size = new System.Drawing.Size(120, 52);
@@ -187,12 +205,25 @@ namespace CheckersWindowsApp
             this.buttonStartGame.UseVisualStyleBackColor = true;
             this.buttonStartGame.Click += new System.EventHandler(this.buttonStartGame_Click);
             // 
+            // labelPlayerOneName
+            // 
+            this.labelPlayerOneName.AutoSize = true;
+            this.labelPlayerOneName.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlayerOneName.ForeColor = System.Drawing.Color.Black;
+            this.labelPlayerOneName.Location = new System.Drawing.Point(36, 40);
+            this.labelPlayerOneName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPlayerOneName.Name = "labelPlayerOneName";
+            this.labelPlayerOneName.Size = new System.Drawing.Size(169, 19);
+            this.labelPlayerOneName.TabIndex = 0;
+            this.labelPlayerOneName.Text = "First player\'s name:";
+            // 
             // FormGameSettings
             // 
             this.AcceptButton = this.buttonStartGame;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 338);
+            this.BackgroundImage = global::CheckersWindowsApp.Properties.Resources.dark_wood_texture;
+            this.ClientSize = new System.Drawing.Size(520, 338);
             this.Controls.Add(this.buttonStartGame);
             this.Controls.Add(this.groupBoxPlayersNames);
             this.Controls.Add(this.groupBoxBoardSize);
@@ -211,8 +242,6 @@ namespace CheckersWindowsApp
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelPlayerOneName;
         private System.Windows.Forms.Label labelPlayerTwoName;
         private System.Windows.Forms.TextBox textBoxPlayerOneName;
         private System.Windows.Forms.TextBox textBoxPlayerTwoName;
@@ -224,5 +253,6 @@ namespace CheckersWindowsApp
         private System.Windows.Forms.RadioButton radioButtonAgainstComputer;
         private System.Windows.Forms.RadioButton radioButtonAgainstHuman;
         private System.Windows.Forms.Button buttonStartGame;
+        private System.Windows.Forms.Label labelPlayerOneName;
     }
 }
